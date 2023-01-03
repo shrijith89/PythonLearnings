ce5bd437eeafae4d2bc68b2eb4389abf71596123
P S Shrijith <p.shrijith@talview.com>
2023-01-03 11:06:29 +0530

    Update HotelBillPayment.py

--- HotelBillPayment.py
+++ HotelBillPayment.py
@@ -2,6 +2,6 @@ bill = int(input("What is your total bill"))
 tipPercentage = int(input("Enter the percentage of tip amount"))
 tipAmount = (tipPercentage/100)*bill
 
-finalBill = bill-tipAmount
+finalBill = bill+tipAmount
 dividedByhowMany = int(input("How many people to be divided to"))
 print(f"The amount for each person is {finalBill/dividedByhowMany}")
