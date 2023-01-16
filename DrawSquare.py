218f32f96eb6e524f69f0e6ef9881de1a496f4f0
P S Shrijith <p.shrijith@talview.com>
2023-01-16 22:13:13 +0530

    drawSquare

---
+++ DrawSquare.py
@@ -0,0 +1,15 @@
+import turtle
+from turtle import *
+
+screen = Screen()
+
+turtle.forward(50)
+turtle.back(100)
+turtle.right(90)
+turtle.back(100)
+turtle.right(180)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+screen.exitonclick()
