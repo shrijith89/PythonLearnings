f3a70a53ba8fa072caf164c990b5bcfc2d24a983
P S Shrijith <shrijithps89@gmail.com>
2023-01-17 09:05:45 +0530

    Delete PrettyTable.py

--- PrettyTable.py
+++
@@ -1,9 +0,0 @@
-import prettytable
-
-prettytable.PrettyTable
-
-table = prettytable.PrettyTable()
-table.add_column("name",["test01","test02","test03"])
-table.add_column("place",["UK","US","Russia"])
-
-print(table)
