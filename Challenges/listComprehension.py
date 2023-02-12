6ede8634ba31bb868307a2e84b515478ef75b7c4
P S Shrijith <shrijithps89@gmail.com>
2023-02-12 23:48:14 +0530

    listComprehension Hackerrank Solution

---
+++ Challenges/listComprehension.py
@@ -0,0 +1,8 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+lst = [[x1, y1, z1] for x1 in range(x+1) for y1 in range(y+1) for z1 in range(z+1) if x1 + y1 + z1 != n]
+
+print(lst)
